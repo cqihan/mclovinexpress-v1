997e0eb7cc550f4e3fe15aa3d2ab4dab2523fd63
cqihan <qihanchan_97@hotmail.com>
2023-07-07 16:05:14 +0800

    update more pics

--- src/pages/phone-photography.js
+++ src/pages/phone-photography.js
@@ -17,28 +17,6 @@ export default function Phone() {
 
         <div className="phone-container">
           <div className={`phone-content-1`}>
-            <h2>Taiwan - Sakura</h2>
-
-            <div className={`phone-1-item`}>
-              <Image.PreviewGroup
-                preview={{
-                  onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
-                }}
-              >
-                <Image alt="pucceu / phone photography" //width={300} 
-                  src="/assets/photography/2019-01.png" className={`phone-photo-item`}
-                />
-                <Image alt="pucceu / phone photography" //width={300} 
-                  src="/assets/photography/2019-02.png" className={`phone-photo-item`}
-                />
-                <Image alt="pucceu / phone photography" //width={300} 
-                  src="/assets/photography/2019-03.png" className={`phone-photo-item`}
-                />
-              </Image.PreviewGroup>
-            </div>
-          </div>
-
-          <div className={`phone-content-1`}>
             <h2>Penang - Butterfly Farm</h2>
 
             <div className={`phone-1-item`}>
@@ -70,6 +48,75 @@ export default function Phone() {
                 />
                 <Image alt="pucceu / phone photography" //width={300} 
                   src="/assets/photography/2018-08.png" className={`phone-photo-item`}
+                />
+              </Image.PreviewGroup>
+            </div>
+          </div>
+
+          <div className={`phone-content-1`}>
+            <h2>Taiwan - Sakura</h2>
+
+            <div className={`phone-1-item`}>
+              <Image.PreviewGroup
+                preview={{
+                  onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
+                }}
+              >
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-01-01.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-01-02.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-01-03.png" className={`phone-photo-item`}
+                />
+              </Image.PreviewGroup>
+            </div>
+          </div>
+
+          <div className={`phone-content-1`}>
+            <h2>Taiwan - Random</h2>
+
+            <div className={`phone-1-item`}>
+              <Image.PreviewGroup
+                preview={{
+                  onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
+                }}
+              >
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-01.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-02.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-03.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-04.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-05.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-06.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-07.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-08.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-09.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-10.png" className={`phone-photo-item`}
+                />
+
+                <Image alt="pucceu / phone photography" //width={300} 
+                  src="/assets/photography/2019-02-11.png" className={`phone-photo-item`}
                 />
               </Image.PreviewGroup>
             </div>
