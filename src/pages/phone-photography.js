0c6f2770d0e5544ff28018b75df186a536bc76bb
cqihan <qihanchan_97@hotmail.com>
2024-01-05 00:43:01 +0800

    added SK photos

--- src/pages/phone-photography.js
+++ src/pages/phone-photography.js
@@ -17,6 +17,76 @@ export default function Phone() {
 
         <div className="phone-container">
           <div className={`phone-content-1`}>
+            <h2>South Korea</h2>
+
+            <div className={`phone-1-item`}>
+              <Image.PreviewGroup
+                preview={{
+                  onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
+                }}
+              >
+                <Image alt="pucceu / phone photography"
+                  src="/assets/photography/2023-SKorea/1.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/2.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography"
+                  src="/assets/photography/2023-SKorea/3.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/4.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/5.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography"
+                  src="/assets/photography/2023-SKorea/6.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/7.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/8.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/9.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/10.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/11.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/12.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/13.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/14.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/15.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/16.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/17.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/18.png" className={`phone-photo-item`}
+                />
+                <Image alt="pucceu / phone photography" 
+                  src="/assets/photography/2023-SKorea/19.png" className={`phone-photo-item`}
+                />
+              </Image.PreviewGroup>
+            </div>
+          </div>
+
+          <div className={`phone-content-1`}>
             <h2>Penang - Butterfly Farm</h2>
 
             <div className={`phone-1-item`}>
